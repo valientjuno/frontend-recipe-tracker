22ac836b3d132b88aa7c225fd8c839caa663b997
valientjuno <jessedoake91@gmail.com>
2025-10-30 13:33:25 -0600

    footer style commit

--- script.js
+++ script.js
@@ -218,9 +218,8 @@ logoutBtn.addEventListener("click", () => {
   msg.textContent = "";
 });
 
-
 // ===== Footer Dynamic Year =====
 const yearSpan = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
-
+yearSpan.textContent = new Date().getFullYear();
